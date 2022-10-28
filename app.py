812c9a0b45f3e3b69f7e05868f5c6003644c7ad7
Felipe Nunes <nunesfelipe5@gmail.com>
2022-10-27 23:14:22 -0300

    Atualizando variavel de ambiente

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
-    
+    updater.bot.setWebhook('https://telebot-py.herokuapp.com/' + TOKEN)
+
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
