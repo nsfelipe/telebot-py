67e465eb31a7571614d1700f10025685f74add45
Felipe Nunes <nunesfelipe5@gmail.com>
2022-10-27 23:06:17 -0300

    Primeiro deploy

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import os
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+PORT = int(os.environ.get('PORT', 5000))
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -54,8 +56,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
