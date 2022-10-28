67f4ac7ee45095d303a14820a7e343bdd3e3f02f
Felipe Nunes <nunesfelipe5@gmail.com>
2022-10-27 23:27:48 -0300

    buscando variavel e ambiente

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = '5598798983:AAEbi4V6K87YUcjfKyQSmhQVyF7bwGVyqyw'
+TOKEN = os.environ['TOKEN']
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
