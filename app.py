00192abcdee6502fd0f03ad09b3b7390acae4d20
Felipe Nunes <nunesfelipe5@gmail.com>
2022-11-02 22:21:06 -0300

    Att url da webhook

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=TOKEN,
-                          webhook_url='ttps://telebot-py.herokuapp.com/' + TOKEN)
+                          webhook_url='https://telebot-py.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
